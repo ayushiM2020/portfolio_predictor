6432f4e83ffd23e58ce19ba4c828e846fa8d9146
protimab <banerj50>
2023-01-21 15:30:09 -0500

    button

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
       <p className="para"> InvestX </p>
-        <button>CLICK ME</button>
+        <button className="click"> CLICK ME </button>
         <p className="para-2">tips</p>
       </header>   
     </div>
