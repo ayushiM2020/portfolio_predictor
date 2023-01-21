0d55e9d324b288741e70243d8e32466abecb3c5d
ayushiM2020 <89742259+ayushiM2020@users.noreply.github.com>
2023-01-21 17:00:33 -0500

    all the work

--- client/src/Insight.js
+++ client/src/Insight.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Insight() {
-  return <div>Insight</div>;
+  return (
+    <div>
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
 
 export default Insight;
